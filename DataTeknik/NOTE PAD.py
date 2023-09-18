e2eab3cc02cf3684bd71742f3f6e8a2eb3e0d50d
axelschelander <axelsche@kth.se>
2023-09-18 13:54:12 +0200

    v1

---
+++ DataTeknik/NOTE PAD.py
@@ -0,0 +1,44 @@
+# Tilldelning
+age = 18
+length = 180
+weight = 80, "kg"
+
+my_coursing_age = age
+
+a = 3
+b = a+15
+#b= 18, men om a ändras i koden ändras b också
+
+print(weight)
+
+a = 12
+b = a
+b = b + b
+c = a + b + 3
+d = c - b
+
+24+12+3 == c
+d= 39 - 24
+d= 15
+
+namn1 = "oskar" #måste ha "" för at vissa python at oskar är en sträng med text
+
+#decimaltal = float värden, skirvs med punkt
+
+number1 = 8.2
+
+
+djur = 'katt'
+a = djur
+b = 'djur'
+
+a = "djur"
+b='djur'
+
+
+x = 15
+print(x)
+print(1001)
+print("Det var lite siffror, detta är en sträng")
+
+print("Alex is", 19, "years old")
