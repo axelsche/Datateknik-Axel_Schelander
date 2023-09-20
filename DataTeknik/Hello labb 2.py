18831459ad0d77941e82c392017419ba8eff72aa
axelschelander <axelsche@kth.se>
2023-09-20 22:13:15 +0200

    lab 2 v2,  working

--- DataTeknik/Hello labb 2.py
+++ DataTeknik/Hello labb 2.py
@@ -1,66 +1,62 @@
-a1 = float(1)
-d=float(1)
-n=float(1)
-q=float(1)
-g1=float(1)
-def series_sum_arithmatic(a1,d,n): #aritmatic_sequence
-    series_sum = (n * (2 * a1 + (n - 1) * d)) / 2
-    return series_sum
-def series_sum_geometric(g1: object, q: object, n: object) -> object: #geometic_series
-    if q == 1: # when ratio is one number g1 won't change so assign sepcial case
-        series_sum_geometric(g1, q, n) == g1 * n
-        return series_sum_geometric(g1, q, n)
+def series_sum_arithmetic(a1, d, n):
+    Aseries_sum = (n * (2 * a1 + (n - 1) * d)) / 2
+    return Aseries_sum
+def series_sum_geometric(g1, q, n):
+    if q == 1:
+        return g1 * n
     else:
-        ssg = float(g1) * (1 - float(q) ** float(n)) / (1 - float(q))
-        return series_sum_geometric
+        ssg = g1 * (1 - q ** n) / (1 - q)
+        return ssg
 
 while True:
-    a1 = input("Skriv in startvärdet (a1)")
-
-    if a1.replace('.', '', 1).isdigit():  # Check if the input is a valid float
-        a1 = float(a1)
+    a1_input = input("Skriv in startvärdet (a1): ")
+    try:
+        a1 = float(a1_input) #För att lösa mitt problem med at a1 lästes som str, omvandlar jag a1 direkt till float genom att ge a1 titeln a1 input i början av koden
         break
-    else:
+    except ValueError:#när fel värdetyp ges i a1_input så ges denna output
         print("Det där var inte ett flyttal. Starta om programmet och försök igen.")
 
 while True:
-    d =input("Skriv in differansen (d)")
-
-    if d.replace('.', '', 1).isdigit():  # Check if the input is a valid float
-        d = float(d)
+    d_input = input("Skriv in differansen (d): ")
+    try:
+        d = float(d_input)
         break
-    else:
+    except ValueError:
         print("Det där var inte ett flyttal. Starta om programmet och försök igen.")
 
 print(" ")
 
 while True:
-    g1 = input("Skriv in startvärdet (g1)")
-
-    if g1.replace('.', '', 1).isdigit():  # Check if the input is a valid float
-        g1 = float(g1)
+    g1_input = input("Skriv in startvärdet (g1): ")
+    try:
+        g1 = float(g1_input)
         break
-    else:
+    except ValueError:
         print("Det där var inte ett flyttal. Starta om programmet och försök igen.")
 
 while True:
-    q =input("Skriv in kvoten (q)")
-
-    if q.replace('.', '', 1).isdigit():  # Check if the input is a valid float
-        q = float(q)
+    q_input = input("Skriv in kvoten (q): ")
+    try:
+        q = float(q_input)
         break
-    else:
+    except ValueError:
         print("Det där var inte ett flyttal. Starta om programmet och försök igen.")
 
 print("Antal termer i summorna:")
-n = input("Skriv in antal element i följden (n):")
+while True:
+    n_input = input("Skriv in antal element i följden (n): ")
+    try:
+        n = int(n_input)
+        break
+    except ValueError:
+        print("Det där var inte ett heltal. Starta om programmet och försök igen.")
 
-resultA = series_sum_arithmatic(a1, d, n)
+resultA = series_sum_arithmetic(a1, d, n)
 resultG = series_sum_geometric(g1, q, n)
 
-if float(resultA) > float(resultG):
+if resultA > resultG:
     print("Den aritmetiska summan är störst.")
-elif resultG == ResultA:
-    print("Den aritmatiska och geometriska summan har samma värde.")
+elif resultG == resultA:
+    print("Den aritmetiska och geometriska summan har samma värde.")
 else:
-    print("Den Geometriska summan är störst.")
+    print("Den geometriska summan är störst.")
