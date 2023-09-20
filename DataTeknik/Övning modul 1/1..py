5f44aa29a050f43cc5996de4c9052bdfac8db747
axelschelander <axelsche@kth.se>
2023-09-20 11:31:27 +0200

    Weight callculator

---
+++ DataTeknik/Övning modul 1/1..py
@@ -0,0 +1,18 @@
+#Weight converter
+
+def weight(w):
+    return (w)
+
+def k(a):
+    return(w)
+
+w = input("How much do you weigh?")
+
+type = input("Is that killograms (k) or pounds(l)")
+
+if type.lower() == "k":
+    print(float(w) * 2.2, "lbs")
+else:
+    print(float(w)/2.2, "kg")
+
+print('done')
