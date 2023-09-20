2fbf87bce090ef21c627ad5fc4a5bae4ac48ca6e
axelschelander <axelsche@kth.se>
2023-09-20 12:22:59 +0200

    Lab 1 with extra

--- DataTeknik/Hello labb 1.py
+++ DataTeknik/Hello labb 1.py
@@ -1,28 +1,27 @@
-"""Ett program som illustrerar funktionsuppdelningen i att gå"""
+def a(a1,d,n): #aritmatic_sequence
+    series_sum = (float(n) * (2 * float(a1) + (float(n) - 1) * float(d))) / 2
+    return (series_sum)
+def g(g1, q, n): #geometic_series
+    if q == 1: # when ratio is one number g1 won't change so assign sepcial case
+        return g1 * n
+    else:
+        series_sum = float(g1) * (1 - float(q) ** float(n)) / (1 - float(q))
+        return series_sum
+    return (sum_gs)
 
-def take_step_fwd():
-    """En funktion som anropar funktionerna som behövs för att ta ett steg"""
-    lift_leg("vänster")
-    lean_body("framåt")
-    lift_leg("höger")
-    lean_body("bakåt")
+choice = input("Do you want to callculate a arithmatic(a) or geometric(g) sequence/series")
+if choice.lower() == "a":
+    a1 = input ("What is the first term(a1)?")
+    d = input("What is the difference(d)?")
+    n = input("what is the number(n) of terms?")
+    result_as = a(a1, d, n)
+    print("Den aritmetiska summan är: ",result_as)
 
-def lift_leg(the_leg):
-    """Lyfter ett ben, the_leg anger höger eller vänster"""
-    print(f"lyft {the_leg} ben")
+else:
+    g1 = input("What is the first term(g1)?")
+    q = input("What is the factor(q)?")
+    n = input("what is the number(n) of terms?")
+    result_gs = g(g1, q, n)
+    print("Den geometriska summan är: ",result_gs)
 
-def lean_body(direction):
-    """Luta kroppen, direction anger riktning"""
-    print(f"luta kroppen {direction}")
-
-def walk_two_steps():
-    """En funktion som tar två steg framåt"""
-    take_step_fwd()
-    take_step_fwd()
-
-walk_two_steps()
-
-
-
-#diska (handdisk, då diskmaskinen inte är så utmanande, vi såg en algoritm på föreläsningen).
 
