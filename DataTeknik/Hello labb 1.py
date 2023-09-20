0499fa602be34d59d55c19a931fe0aa7066727e7
axelschelander <axelsche@kth.se>
2023-09-20 08:47:40 +0200

    area practice

--- DataTeknik/Hello labb 1.py
+++ DataTeknik/Hello labb 1.py
@@ -1,14 +1,23 @@
 def square(x):
     return (x*x)
 
+def circle(x):
+    return(x*x*3.14)
+
 x = 0
 
-user_input = input("Enter a number: ")
+input_shape = input("what shape is the object? square or circle")
+user_input = input("what is the radius/side length: ")
 
 # Converting the input string to an integer
 x = int(user_input)
 
+areaS = (x*x)
+areaC = (x*x*3.14)
 
-area = (x*x)
+if input_shape == square:
+    print (areaS)
+else:
+    print(areaC)
 
-print (area)
+
