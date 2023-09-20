29a81244afeedbf5f3015dde666bf06e1b7c4818
axelschelander <axelsche@kth.se>
2023-09-20 08:27:33 +0200

    test

--- DataTeknik/Hello labb 1.py
+++ DataTeknik/Hello labb 1.py
@@ -1,1 +1,13 @@
-print("hello world!")
+def 4_square(x*x):
+    return (x*x)
+
+x = 0
+
+input = float(input("what is the side length: "))
+
+input = float(x)
+
+area = (x*x)
+
+print (area)
+print (x)
