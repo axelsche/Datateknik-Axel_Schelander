15ad21d7621f5d157d35755ffff91549f318fecf
axelschelander <axelsche@kth.se>
2023-09-22 13:14:20 +0200

    lab 3 v2,  working

---
+++ DataTeknik/Hello labb 3.py
@@ -0,0 +1,40 @@
+#I denna laboration kommer vi använda oss av en egenskriven modul som vi importerar in i vårt huvudprogram.
+#Denna modul ska innehålla två funktioner, en som kontrollerar att inmatningen är ett heltal (int) och en som,
+#kontrollerar att inmatningen är ett flyttal (float). De två funktionerna ska ta inmatning från användaren och inte,
+#returnera förrän användaren har matat in korrekt data.
+def series_sum_arithmetic(A1, D, N):
+    Aseries_sum = (N * (2 * A1 + (N - 1) * D)) / 2
+    return Aseries_sum
+def series_sum_geometric(G1, Q, N):
+    if Q == 1:
+        return G1 * N
+    else:
+        ssg = G1 * (1 - Q ** N) / (1 - Q)
+        return ssg
+
+import float_input
+
+import float_input
+print("variabler för den aritmetiska summan:")
+A1 = float_input.get_float_input("A1: ")
+D = float_input.get_float_input("D: ")
+
+G1 = float_input.get_float_input("G1: ")
+Q = float_input.get_float_input("Q: ")
+
+
+import integer_input
+print ("Antal termer i summorna:")
+N = integer_input.get_integer_input()
+
+
+resultA = series_sum_arithmetic(A1, D, N)
+resultG = series_sum_geometric(G1, Q, N)
+
+#storleksjämföring av aritmatisk och geometrisk
+if resultA > resultG:
+    print("Den aritmetiska summan är störst.")
+elif resultG == resultA:
+    print("Den aritmetiska och geometriska summan har samma värde.")
+else:
+    print("Den geometriska summan är störst.")
--- DataTeknik/Övning modul 1/test.py
+++ DataTeknik/Övning modul 1/test.py
@@ -1,12 +1,19 @@
-def arithmetic_series_sum(a1, d, n):
-    # Calculate the sum of the arithmetic series
-    series_sum = n * (2 * a1 + (n - 1) * d) / 2
-    return series_sum
+if 0==0:
+    a1 = input("What is the first term(a1)?")
+    d = input("What is the difference(d)?")
+    print(" ")
+    g1 = input("What is the first term(g1)?")
+    q = input("What is the factor(q)?")
+    print(" ")
 
-# Example usage:
-a1 = 1.0  # First term (float)
-d = 2.0   # Common difference (float)
-n = 5.0   # Number of terms (float)
+n = input("what is the number(n) of terms?")
 
-result = arithmetic_series_sum(a1, d, n)
-print("Sum of the arithmetic series:", result)
+result_gs = series_sum_geometric(g1, q, n)
+result_as = series_sum_arithmatic(a1, d, n)
+
+if n == int:
+    print("Aritmatisk summa ",result_as)
+    print("Geometrisk summa",result_gs)
+else:
+    n = input("thats not a whole number, please enter a whole number(n) agian?")
+
